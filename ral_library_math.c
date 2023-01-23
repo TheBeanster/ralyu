12b63a3bb201b74a005a4a33e13d8f01ee0fe643
TheBeanster <filip.lindstrom050131@gmail.com>
2023-01-23 22:57:10 +0100

    Made the math library a little bit more terse

--- ral_library_math.c
+++ ral_library_math.c
@@ -9,61 +9,67 @@
 
 
 
-#define MATHFUNC_1ARG(base_c_function)								\
-	if (arguments->itemcount != 1) return NULL;						\
-																	\
-	Ral_Object* arg = arguments->begin;								\
-	if (!arg) return NULL;											\
-	if (arg->type != Ral_TYPE_NUMBER) return NULL;					\
-																	\
-	return Ral_CreateNumberObject(base_c_function(arg->val.number))
+#define MATHFUNC_1ARG(base_c_function)														\
+	(Ral_State* const state, Ral_List* const arguments)										\
+	{																						\
+		if (arguments->itemcount != 1) return NULL;											\
+																							\
+		Ral_Object* arg = arguments->begin;													\
+		if (!arg) return NULL;																\
+		if (arg->type != Ral_TYPE_NUMBER) return NULL;										\
+																							\
+		return Ral_CreateNumberObject(base_c_function(arg->val.number));					\
+	}
 
 
 
-#define MATHFUNC_2ARGS(base_c_function)								\
-	if (arguments->itemcount != 2) return NULL;						\
-																	\
-	Ral_Object* arg1 = arguments->begin;							\
-	if (!arg1) return NULL;											\
-	if (arg1->type != Ral_TYPE_NUMBER) return NULL;					\
-	Ral_Object* arg2 = arguments->begin->next;						\
-	if (!arg2) return NULL;											\
-	if (arg2->type != Ral_TYPE_NUMBER) return NULL;					\
-																	\
-	return Ral_CreateNumberObject(base_c_function(arg1->val.number, arg2->val.number))
+#define MATHFUNC_2ARGS(base_c_function)														\
+	(Ral_State* const state, Ral_List* const arguments)										\
+	{																						\
+		if (arguments->itemcount != 2) return NULL;											\
+																							\
+		Ral_Object* arg1 = arguments->begin;												\
+		if (!arg1) return NULL;																\
+		if (arg1->type != Ral_TYPE_NUMBER) return NULL;										\
+		Ral_Object* arg2 = arguments->begin->next;											\
+		if (!arg2) return NULL;																\
+		if (arg2->type != Ral_TYPE_NUMBER) return NULL;										\
+																							\
+		return Ral_CreateNumberObject(base_c_function(arg1->val.number, arg2->val.number));	\
+	}
 
 
 
-static Ral_Object* stdmath_sin  (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(sin); }
-static Ral_Object* stdmath_cos  (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(cos); }
-static Ral_Object* stdmath_tan  (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(tan); }
-static Ral_Object* stdmath_sinh (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(sinh); }
-static Ral_Object* stdmath_cosh (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(cosh); }
-static Ral_Object* stdmath_tanh (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(tanh); }
-static Ral_Object* stdmath_asin (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(asin); }
-static Ral_Object* stdmath_acos (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(acos); }
-static Ral_Object* stdmath_atan (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(atan); }
-static Ral_Object* stdmath_asinh(Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(asinh); }
-static Ral_Object* stdmath_acosh(Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(acosh); }
-static Ral_Object* stdmath_atanh(Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(atanh); }
+static Ral_Object* stdmath_sin		MATHFUNC_1ARG(sin);
+static Ral_Object* stdmath_cos		MATHFUNC_1ARG(cos);
+static Ral_Object* stdmath_tan		MATHFUNC_1ARG(tan);
+static Ral_Object* stdmath_sinh		MATHFUNC_1ARG(sinh);
+static Ral_Object* stdmath_cosh		MATHFUNC_1ARG(cosh);
+static Ral_Object* stdmath_tanh		MATHFUNC_1ARG(tanh);
+static Ral_Object* stdmath_asin		MATHFUNC_1ARG(asin);
+static Ral_Object* stdmath_acos		MATHFUNC_1ARG(acos);
+static Ral_Object* stdmath_atan		MATHFUNC_1ARG(atan);
+static Ral_Object* stdmath_asinh	MATHFUNC_1ARG(asinh);
+static Ral_Object* stdmath_acosh	MATHFUNC_1ARG(acosh);
+static Ral_Object* stdmath_atanh	MATHFUNC_1ARG(atanh);
 
-static Ral_Object* stdmath_exp  (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(exp); }
-static Ral_Object* stdmath_ln   (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(log); }
-static Ral_Object* stdmath_log  (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(log10); }
-static Ral_Object* stdmath_log2 (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(log2); }
-static Ral_Object* stdmath_sqrt (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(sqrt); }
-static Ral_Object* stdmath_cbrt (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(cbrt); }
-static Ral_Object* stdmath_round(Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(round); }
-static Ral_Object* stdmath_floor(Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(floor); }
-static Ral_Object* stdmath_ceil (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(ceil); }
-static Ral_Object* stdmath_abs  (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(fabs); }
-static Ral_Object* stdmath_trunc(Ral_State* const state, Ral_List* const arguments) { MATHFUNC_1ARG(trunc); }
+static Ral_Object* stdmath_exp		MATHFUNC_1ARG(exp);
+static Ral_Object* stdmath_ln		MATHFUNC_1ARG(log);
+static Ral_Object* stdmath_log		MATHFUNC_1ARG(log10);
+static Ral_Object* stdmath_log2		MATHFUNC_1ARG(log2);
+static Ral_Object* stdmath_sqrt		MATHFUNC_1ARG(sqrt);
+static Ral_Object* stdmath_cbrt		MATHFUNC_1ARG(cbrt);
+static Ral_Object* stdmath_round	MATHFUNC_1ARG(round);
+static Ral_Object* stdmath_floor	MATHFUNC_1ARG(floor);
+static Ral_Object* stdmath_ceil		MATHFUNC_1ARG(ceil);
+static Ral_Object* stdmath_abs		MATHFUNC_1ARG(fabs);
+static Ral_Object* stdmath_trunc	MATHFUNC_1ARG(trunc);
 
-static Ral_Object* stdmath_atan2(Ral_State* const state, Ral_List* const arguments) { MATHFUNC_2ARGS(atan2); }
+static Ral_Object* stdmath_atan2	MATHFUNC_2ARGS(atan2);
 
-static Ral_Object* stdmath_pow  (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_2ARGS(pow); }
-static Ral_Object* stdmath_mod  (Ral_State* const state, Ral_List* const arguments) { MATHFUNC_2ARGS(fmod); }
-static Ral_Object* stdmath_hypot(Ral_State* const state, Ral_List* const arguments) { MATHFUNC_2ARGS(hypot); }
+static Ral_Object* stdmath_pow		MATHFUNC_2ARGS(pow);
+static Ral_Object* stdmath_mod		MATHFUNC_2ARGS(fmod);
+static Ral_Object* stdmath_hypot	MATHFUNC_2ARGS(hypot);
 
 
 
